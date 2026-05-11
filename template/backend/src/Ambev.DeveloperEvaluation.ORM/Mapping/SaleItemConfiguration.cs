8c5e778f66ecaa9983ba2ca0b14bfa8adb599ae8
Leonardo Couto Trindade <leonardo.trindade@performait.com>
2026-05-11 11:49:16 -0300

    feat(orm): add database mappings and repository implementation for sales

---
+++ template/backend/src/Ambev.DeveloperEvaluation.ORM/Mapping/SaleItemConfiguration.cs
@@ -0,0 +1,24 @@
+using Ambev.DeveloperEvaluation.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Ambev.DeveloperEvaluation.ORM.Mapping;
+
+public class SaleItemConfiguration : IEntityTypeConfiguration<SaleItem>
+{
+    public void Configure(EntityTypeBuilder<SaleItem> builder)
+    {
+        builder.ToTable("SaleItems");
+
+        builder.HasKey(i => i.Id);
+        builder.Property(i => i.Id).HasColumnType("uuid").HasDefaultValueSql("gen_random_uuid()");
+
+        builder.Property(i => i.SaleId).IsRequired().HasColumnType("uuid");
+        builder.Property(i => i.Product).IsRequired().HasMaxLength(255);
+        builder.Property(i => i.Quantity).IsRequired();
+        builder.Property(i => i.UnitPrice).IsRequired().HasColumnType("decimal(18,2)");
+        builder.Property(i => i.Discount).IsRequired().HasColumnType("decimal(18,2)");
+        builder.Property(i => i.TotalAmount).IsRequired().HasColumnType("decimal(18,2)");
+        builder.Property(i => i.IsCancelled).IsRequired();
+    }
+}
